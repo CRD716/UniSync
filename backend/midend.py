c08c527e5bea7eca33c91af5b804e77badbdc562
crd716 <crd716@gmail.com>
2023-09-16 17:14:33 -0500

    sob

--- backend/midend.py
+++ backend/midend.py
@@ -1,2 +1,6 @@
 from llama_cpp import Llama
-llm = Llama(model_path="./models/7B/llama-model.gguf")
+llm = Llama(model_path="backend/models/marcoroni-13b.Q4_K_S.gguf")
+
+output = llm("Q: Name the planets in the solar system? A: ", max_tokens=64, stop=["Q:", "\n"], echo=True)
+
+print(output)

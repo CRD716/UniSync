4e2fc19c0b53a5f422c4285715eec52506db33e5
crd716 <crd716@gmail.com>
2023-09-16 21:42:34 -0500

    cooking

--- backend/midend.py
+++ backend/midend.py
@@ -1,6 +1,37 @@
 from llama_cpp import Llama
-llm = Llama(model_path="backend/models/marcoroni-13b.Q4_K_S.gguf")
+from flask import Flask,request,jsonify
 
-output = llm("Q: Name the planets in the solar system? A: ", max_tokens=64, stop=["Q:", "\n"], echo=True)
+#llama setup
+llm = Llama(model_path="backend/models/marcoroni-13b.Q4_K_S.gguf", n_ctx=4096, low_vram=True)
 
-print(output)
+app = Flask(__name__)
+
+masterdict = {
+  "AAAAA": {
+      "post1": {
+        "name" : "john doe",
+        "txt" : "In the beginning God created the heavens and the earth."
+      },
+      "post2" : {
+        "name" : "jane doe",
+        "txt" : "Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters."
+      }
+  }
+}
+
+@app.route("/submit-text", methods=["POST"])
+def submittext():
+    # incoming as giant text block, add instructions
+    prompt = "### Instruction:\nSummarize the following collection of notes.\n"
+
+
+#debug api
+@app.route("/generate/<prompt>")
+def generate(prompt):
+    prompt = "### Instruction:\n"+prompt+"\n### Response:\n"
+
+    output = llm(prompt, max_tokens=256, temperature=0.5, mirostat_mode=2, stop=["\n"], echo=True)
+    print(output)
+    return output, 200
+
+app.run(debug=True)

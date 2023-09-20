152fbb7d3df9ac2142b8dcd0593beffe8c16ffee
Turtlecap1324 <72718544+Ashwin-Iyer1@users.noreply.github.com>
2023-09-19 22:58:02 -0500

    GIT MAKES ME WANT TO KMS

--- frontend/hacksmu/src/ApiAccess.js
+++ frontend/hacksmu/src/ApiAccess.js
@@ -1,23 +1,40 @@
-import Login from "./Login.js"
-import Modal from "./Modal.js"
-function postData(nam, code, data) {
-  console.log(JSON.stringify({name: nam, roomid: code, txt: data }));
-  const response = fetch(' https://termite-working-logically.ngrok-free.app/submit-text', {
+export function postData(nam, code, data) {
+  console.log(JSON.stringify({ name: nam, roomid: code, txt: data }));
+  const response = fetch('http://localhost:5000/submit-text', {
     method: 'POST',
-    mode: 'no-cors',
-    /*
     headers: {
-      'Content-Type': 'application/json', // Specify the data format being sent
-    },*/
-    body: JSON.stringify({name: nam, roomid: code, txt: data }), // Send data in JSON format
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({ name: nam, roomid: code, text: data }),
   })
-  .then(response => response.json())
-  .then(data => {
-    // Process the response data 
-  })
-  .catch(error => {
-    // Handle errors
-    console.log(error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      // Process the response data
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error);
+    });
 }
-export default postData;
+
+// Function to get notes data
+export function getNotesData(roomId) {
+  return fetch(`http://localhost:5000/get-notes/${roomId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Process the retrieved notes data
+      return data;
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error);
+      return [];
+    });
+}
